5b1722106b02a9cf831d7f52e5a841547fdb87f6
Jack Kelliher <kelljj2@student.op.ac.nz>
2021-09-17 12:43:55 +1200

    bugfixes

--- app/Models/Plane.php
+++ app/Models/Plane.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plane extends Model
 {
-    protected $fillable = ['model' ,'speed', 'capacity'];
+    protected $fillable = ['model' ,'speed', 'capacity', 'airport_id'];
 
     public function airports() {
         return $this->belongsTo(Airport::Class, 'airport_id', 'id');
